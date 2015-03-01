a76c0149551d9723d04992bf5018411f6af62b0d
Tristan Sandras <tristan@sandras.fr>
2015-03-01 15:32:35 +0100

    [Devise] Add account gmail for send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,8 @@ Rubricam::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
