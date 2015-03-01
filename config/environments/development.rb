6472958176f58bed76240cba1df5e503048e920b
Tristan Sandras <tristan@sandras.fr>
2015-03-01 14:55:14 +0100

    [FIXBUG] Add field to users for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Rubricam::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
