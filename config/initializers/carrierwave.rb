022861990e96bb83b8fd14e9ddc9b681e496ff06
Tristan Sandras <tristan@sandras.fr>
2015-04-02 18:20:27 +0200

    Add AWS_BUCKET to env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if !Rails.env.development?
     CarrierWave.configure do |config|
       config.storage    = :aws
-      config.aws_bucket = ENV.fetch('rubricam-avatars')
+      config.aws_bucket = ENV.fetch('AWS_BUCKET')
       config.aws_acl    = :public_read
       config.asset_host = 'http://s3-eu-west-1.amazonaws.com'
       config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
