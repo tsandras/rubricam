7b1a7f2f0794d1a55c004894318f6a65b121dfde
Tristan Sandras <tristan@sandras.fr>
2015-10-08 19:43:13 +0200

    Fixbug routine/objet relation

--- app/models/objets_routines.rb
+++ app/models/objets_routines.rb
@@ -5,8 +5,6 @@
 #  id         :integer          not null, primary key
 #  objet_id   :integer
 #  routine_id :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
 #
 
 class ObjetsRoutines < ActiveRecord::Base
