c8362503e6a97b86dd5f8e6de03b06b3833426e9
Tristan Sandras <tristan@sandras.fr>
2015-10-12 15:19:39 +0200

    Improve index of master models

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,2 +1,14 @@
 module PartiesHelper
+  def label_duree(duree)
+    if duree < 4
+      color = "label-info"
+    elsif duree < 6
+      color = "label-success"
+    elsif duree < 8
+      color = "label-success"
+    else
+      color = "label-danger"
+    end
+    content_tag :span, "#{duree} heures", class: "label #{color}"
+  end
 end
