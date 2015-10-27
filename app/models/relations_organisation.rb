2f48944c865f0fb75ad94b3ed4efefbaf12512a7
Tristan Sandras <tristan@sandras.fr>
2015-10-27 20:50:19 +0100

    :bug: fix bug add scope none_secret to relations_organisation

--- app/models/relations_organisation.rb
+++ app/models/relations_organisation.rb
@@ -1,3 +1,5 @@
 class RelationsOrganisation < ActiveRecord::Base
   attr_accessible :description, :from_organisation_id, :name, :secret, :to_organisation_id
+
+  scope :none_secret, lambda { where("secret = ? or secret is null", false) }
 end
